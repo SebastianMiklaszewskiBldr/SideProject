65fb149e1cfdf6842a0faabdf83645c00538c587
SebastianMiklaszewskiBldr <sebastian.miklaszewski@boldare.com>
2023-09-12 15:12:17 +0200

    add test case

--- api/warehouse/tests/Write/Product/Add/Application/AddProductHandlerTest.php
+++ api/warehouse/tests/Write/Product/Add/Application/AddProductHandlerTest.php
@@ -7,12 +7,21 @@ namespace App\Tests\Write\Product\Add\Application;
 use App\Tests\IntegrationTestCase;
 use App\Tests\Write\Product\Add\AddProductTestData;
 use App\Write\Product\Add\Application\AddProductHandler;
+use LogicException;
 
 final class AddProductHandlerTest extends IntegrationTestCase
 {
     private AddProductTestData $testData;
     private AddProductHandlerTestAssertions $assertions;
     private AddProductHandler $handler;
+
+    public function test_Handle_ShouldThrowException_WhenStockNotFound(): void
+    {
+        $command = $this->testData->getCommand();
+
+        $this->expectException(LogicException::class);
+        $this->handler->handle($command);
+    }
 
     public function test_Handle_ShouldSaveProduct_WhenProductCreatedWithoutExceptions(): void
     {
